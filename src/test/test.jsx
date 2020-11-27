6061d321454fbb44ec3948940a05fc19cf93593a
Bayu Darmawan <darmawanbayu1@gmail.com>
2020-11-27 19:33:05 +0700

    test

---
+++ src/test/test.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class test extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <>
+        hello
+            </>
+         );
+    }
+}
+ 
+export default test;
