a349cce27a7eab21cced799ad100d04f2ba3b38d
Wili0710 <66407599+wili0710@users.noreply.github.com>
2020-12-03 15:22:40 +0700

    upd

--- src/controllers/TransactionControllers.js
+++ src/controllers/TransactionControllers.js
@@ -3,6 +3,7 @@ const {encrypt,transporter,OtpCreate, OtpConfirm,Link_Frontend}=require('../help
 const {createJWToken} = require('../helpers/jwt')
 const fs =require('fs')
 const handlebars=require('handlebars')
+const { parse } = require('path')
 
 const DbPROMselect=(sql)=>{
     return new Promise((resolve,reject)=>{
@@ -29,5 +30,88 @@ module.exports={
         const upload=await DbPROMselect(sql)
         console.log("upload transaksi berhasil")
         return res.send(true)
+    },
+    AddToCart:async(req,res)=>{
+        const {user_id,products_id,parcel_id,qty}=req.body
+        let sql= `select * from transaksi where status='oncart' and users_id=${db.escape(user_id)}`
+        const getdatatransaksi=await DbPROMselect(sql)
+        if (getdatatransaksi.length){
+            sql=`select * from transaksidetail where products_id=${db.escape(products_id)} 
+                and transaksi_id=${db.escape(getdatatransaksi[0].id)} and parcel_id=${db.escape(parcel_id)} and isdeleted=0`
+            const getdatatransaksidetail=await DbPROMselect(sql)
+            if(getdatatransaksidetail.length){
+                let senttosql={
+                    qty:parseInt(getdatatransaksidetail[0].qty)+parseInt(qty)
+                }
+                sql=`update transaksidetail set ${db.escape(senttosql)} where products_id=${db.escape(getdatatransaksidetail[0].products_id)}
+                and transaksi_id=${db.escape(getdatatransaksidetail[0].transaksi_id)}`
+                const updatedata=await DbPROMselect(sql)
+                
+                sql=`select * from transaksidetail td
+                    join transaksi t on td.transaksi_id=t.id
+                    join products p on td.products_id=p.id
+                    join transaksidetail_has_products tdhp
+                    on tdhp.transaksidetail_id=td.id
+                    where t.status='oncart' and t.users_id=1 and td.isdeleted=0;
+                    `
+                const getcart=await DbPROMselect(sql)
+                return res.send(getcart)
+            }else{
+                //Klo produk belum ada di cart
+                let senttosql={
+                    products_id:products_id,
+                    parcel_id:parcel_id,
+                    qty:qty
+                }
+                sql=`insert into transaksidetail set ${db.escape(senttosql)}`
+                const addproductocart=await DbPROMselect(sql)
+                sql=`select * from transaksidetail td
+                    join transaksi t on td.transaksi_id=t.id
+                    join products p on td.products_id=p.id
+                    join transaksidetail_has_products tdhp
+                    on tdhp.transaksidetail_id=td.id
+                    where t.status='oncart' and t.users_id=1 and td.isdeleted=0;
+                    `
+                const datacart=await DbPROMselect(sql)
+                return res.send(datacart)
+            }
+        }else{
+            // klo car masih kosong
+            let senttosql={
+                tanggal:new Date(),
+                status:'oncart',
+                users_id:user_id
+            }
+            sql=`insert into transaksi set ${db.escape(senttosql)}`
+            const addcart=await DbPROMselect(sql)
+            senttosql={
+                products_id:products_id,
+                transaksi_id:addcart.insertId,
+                qty:qty
+            }
+            sql=`insert into transaksidetail set ${db.escape(senttosql)}`
+            const addtotransaksidetail=await DbPROMselect(sql)
+            sql=`select * from transaksidetail td
+                join transaksi t on td.transaksi_id=t.id
+                join products p on td.products_id=p.id
+                join transaksidetail_has_products tdhp
+                on tdhp.transaksidetail_id=td.id
+                where t.status='oncart' and t.users_id=1 and td.isdeleted=0;
+                `
+            const datacart=await DbPROMselect(sql)
+            return res.send(datacart)
+        }
+    },
+    GetCart:async(req,res)=>{
+        const {user_id}=req.query
+        sql=`select * from transaksidetail td
+            join transaksi t on td.transaksi_id=t.id
+            join products p on td.products_id=p.id
+            join transaksidetail_has_products tdhp
+            on tdhp.transaksidetail_id=td.id
+            where t.status='oncart' and t.users_id=1 and td.isdeleted=0;
+            `
+        const getcart=await DbPROMselect(sql)
+        return res.send(getcart)
     },
 }
--- src/routes/TransactionRoutes.js
+++ src/routes/TransactionRoutes.js
@@ -3,6 +3,8 @@ const {TransactionControllers}=require('./../controllers')
 
 // Uji coba, sifatnya sementara
 Router.post('/transaksi',TransactionControllers.transaksi)
+Router.post('/transaksi',TransactionControllers.AddToCart)
+Router.post('/transaksi',TransactionControllers.GetCart)
 
 
 module.exports=Router
