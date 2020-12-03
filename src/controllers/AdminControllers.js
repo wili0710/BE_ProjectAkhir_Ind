9db3b687cf340bcc0c365cd7e697fba76cd4c6c8
Bayu Darmawan <darmawanbayu1@gmail.com>
2020-12-04 02:34:11 +0700

    update

--- src/controllers/AdminControllers.js
+++ src/controllers/AdminControllers.js
@@ -2,7 +2,7 @@ const {db}=require('../connections')
 
 module.exports={
     getAllUser:(req,res)=>{
-        let sql=`select * from users where isdeleted=0`
+        let sql=`select * from users`
         db.query(sql,(err,datausers)=>{
             if(err) return res.status(500).send(err)
             return res.send(datausers)
