3e2ca44dc87772430238dd58b10f762b432516ec
Bayu Darmawan <darmawanbayu1@gmail.com>
2020-12-10 16:35:11 +0700

    update be

--- src/routes/TransactionRoutes.js
+++ src/routes/TransactionRoutes.js
@@ -4,6 +4,6 @@ const {TransactionControllers}=require('./../controllers')
 Router.post('/addtocart',TransactionControllers.AddToCart)
 Router.get('/getcart',TransactionControllers.GetCart)
 Router.post('/removefromcart',TransactionControllers.RemoveFromCart)
-
+Router.post('/addtocartproduct',TransactionControllers.AddToCartProduct)
 
 module.exports=Router
