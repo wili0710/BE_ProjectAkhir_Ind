c6aed6f6878c4f5aa21550824c5fdfec2a02fd6e
Wili0710 <66407599+wili0710@users.noreply.github.com>
2020-12-15 08:23:24 +0700

    redeploy

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,6 +6,6 @@ module.exports={
     transporter     : require('./mailers'),
     OtpCreate       : require('./OtpCreate'),
     OtpConfirm      : require('./OtpConfirm'),
-    Link_Frontend   : "http://localhost:3000",
-    // Link_Frontend   : "http://h2h.wiliromario.com",
+    // Link_Frontend   : "http://localhost:3000",
+    Link_Frontend   : "http://h2h.wiliromario.com",
 }
